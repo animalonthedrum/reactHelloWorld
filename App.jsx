bd8302f16af0434b7ec68a5e41a664ef9c19df3e
Brett Miller <miller.brett.andrew@gmail.com>
2017-08-23 21:54:52 -0500

    testing multiple lines in jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const helloImg = 'https://i.ytimg.com/vi/C2O7lM0bU0g/maxresdefault.jpg';
 
 
@@ -9,7 +10,9 @@ class App extends React.Component {
          <div>
             <h1>Hello World!!!</h1>
             <h2>{5+5}</h2>
-            < img src = {helloImg}/>
+            <img src = {helloImg}/>
+            <p>testing multiple lines of jsx</p>
+            <p>this will render too</p>
 
          </div>
       );
