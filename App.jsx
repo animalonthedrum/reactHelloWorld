7ab68f48db072ac6a7280aeae8d286a34279bd4d
Brett Miller <miller.brett.andrew@gmail.com>
2017-08-24 21:04:47 -0500

    Learning objects and this in a component

--- App.jsx
+++ App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
-const helloImg = 'https://i.ytimg.com/vi/C2O7lM0bU0g/maxresdefault.jpg';
-
+const helloImg = {
+src:'https://i.ytimg.com/vi/C2O7lM0bU0g/maxresdefault.jpg',
+alt:"hello world",
+width:'80%',
+height:'100%'
+};
 
 
 class App extends React.Component {
+get name(){
+  return 'Brett';
+}
    render() {
       return (
          <div>
             <h1>Hello World!!!</h1>
             <h2>{5+5}</h2>
-            <img src = {helloImg}/>
+            <img
+            src = {helloImg.src}
+            alt = {helloImg.alt}
+            width={helloImg.width}
+            height = {helloImg.height}/>
             <p>testing multiple lines of jsx</p>
             <p>this will render too</p>
+            <h1>My name is {this.name}.</h1>
 
          </div>
       );
